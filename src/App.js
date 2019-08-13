57c11bd29571747cecf809ea54cbb56bbca76fe0
DanFCo <danfcorreia@gmail.com>
2019-08-13 12:58:06 -0400

    connected to backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import Brewery from './Brewery'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+state={
+  breweries: []
 }
+
+componentDidMount(){
+  fetch("http://localhost:3000/breweries")
+  .then(response => response.json())
+  .then(data =>{
+    this.setState({
+      breweries: data
+    })
+
+  })
+}
+
+
+
+
+
+
+  render() {
+
+    return (
+      <div>
+        {this.state.breweries.map(brewery =>{
+
+        return <Brewery key={brewery.id} data={brewery} />
+
+        })}
+
+      </div>
+    );
+  }
+
+
+}//---------end of class------------
 
 export default App;
---
+++ src/Brewery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Brewery extends React.Component {
+
+  render() {
+    return (
+      <div>
+    {this.props.data.name}
+
+
+      </div>
+    );
+  }
+
+}
+
+export default Brewery ;
